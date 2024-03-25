524e43bfc11559c5c2923555817bf2af12d6a3e9
bhanudatsinhjhala <jhalabhanudattsinh4090@gmail.com>
2024-03-25 16:50:46 +0530

    fix(app): Refactor the logger and main.ts code
    - Fix the logger file logging code for production env
    - Fix the main file port code

--- src/common/config/logger.config.ts
+++ src/common/config/logger.config.ts
@@ -9,70 +9,45 @@ import { Defaults } from './default.config';
  * @returns
  */
 export const winstonOptions = (): WinstonModuleOptions => {
-  const logsFilePath = path.join(path.resolve(), `./logs/`);
-  let dailyRotateTransport;
+  const transportConfig: winston.transport[] = [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        utilities.format.nestLike('Browser Markdown', {
+          colors: true,
+          prettyPrint: false,
+        }),
+      ),
+    }),
+  ];
+
   if (process.env.NODE_ENV !== 'production') {
-    dailyRotateTransport = new DailyRotateFile({
+    const logsFilePath = path.join(path.resolve(), `./logs/`);
+    const dailyRotateTransport = new DailyRotateFile({
       filename: logsFilePath + '%DATE%.log',
       datePattern: 'YYYY-MM-DD',
     });
-  }
 
+    transportConfig.push(
+      ...[
+        new winston.transports.File({
+          filename: Defaults.COMBINED_LOG_PATH,
+          maxsize: 1048576,
+        }),
+        new winston.transports.File({
+          filename: Defaults.ERROR_LOG_PATH,
+          level: 'error',
+          maxsize: 83_88_608,
+        }),
+      ],
+      dailyRotateTransport,
+    );
+  }
   return {
     exitOnError: false,
     level: 'debug',
     format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.json()),
-    transports: [
-      ...(process.env.NODE_ENV === 'production'
-        ? [
-            new winston.transports.Console({
-              format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-                utilities.format.nestLike('Browser Markdown', {
-                  colors: true,
-                  prettyPrint: false,
-                }),
-              ),
-            }),
-          ]
-        : [
-            new winston.transports.File({
-              filename: Defaults.COMBINED_LOG_PATH,
-              maxsize: 1048576,
-            }),
-            new winston.transports.File({
-              filename: Defaults.ERROR_LOG_PATH,
-              level: 'error',
-              maxsize: 83_88_608,
-            }),
-            ...(dailyRotateTransport !== undefined
-              ? [
-                  dailyRotateTransport,
-                  new winston.transports.Console({
-                    format: winston.format.combine(
-                      winston.format.colorize(),
-                      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-                      utilities.format.nestLike('Browser Markdown', {
-                        colors: true,
-                        prettyPrint: false,
-                      }),
-                    ),
-                  }),
-                ]
-              : [
-                  new winston.transports.Console({
-                    format: winston.format.combine(
-                      winston.format.colorize(),
-                      winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-                      utilities.format.nestLike('Browser Markdown', {
-                        colors: true,
-                        prettyPrint: false,
-                      }),
-                    ),
-                  }),
-                ]),
-          ]),
-    ],
+    transports: transportConfig,
   };
 };
